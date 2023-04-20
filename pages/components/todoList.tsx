5f5cf1f1ce77bf52ae93a9b2f71db0408d91d5cb
masac <msktjm98@gmail.com>
2023-04-20 17:44:32 +0900

    ステータスの色分けできた

--- pages/components/todoList.tsx
+++ pages/components/todoList.tsx
@@ -24,6 +24,7 @@ import {
   Thead,
   Tr,
   VStack,
+  color,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
@@ -34,16 +35,16 @@ export const TodoList = () => {
   useEffect(() => {
     const todoData = collection(db, "todos");
     getDocs(todoData).then((snapshot) => {
-      const todoList: Todo[] = []
+      const todoList: Todo[] = [];
       snapshot.docs.map((doc) => {
         const todo: Todo = {
           id: doc.id,
           title: doc.data().title,
           content: doc.data().content,
           isDone: doc.data().isDone,
-        }
+        };
         todoList.push(todo);
-      })
+      });
       // console.log(snapshot.docs.map((doc) => ({...doc.data()})))
       setTodos(todoList);
     });
@@ -65,39 +66,27 @@ export const TodoList = () => {
           <TableCaption>説明は特になし</TableCaption>
           <Thead>
             <Tr backgroundColor="gray.200">
+              <Th>(ステータス)</Th>
               <Th>(件名)</Th>
               <Th>(内容)</Th>
-              <Th>(ステータス)</Th>
               <Th>(備考)</Th>
             </Tr>
           </Thead>
           <Tbody>
             {todos.map((todo) => (
               <Tr key={todo.id}>
+                <Td className="tdStyle">
+                  {todo.isDone === false ? (
+                    <Box color="red">未完了</Box>
+                  ) : (
+                    <Box color="blue">完了</Box>
+                  )}
+                </Td>
                 <Td>{todo.title}</Td>
                 <Td>{todo.content}</Td>
-                <Td>{todo.isDone === false ? '未完了' : '完了'}</Td>
                 <Td></Td>
               </Tr>
             ))}
-            <Tr>
-              <Td>ログイン・ログアウト機能実装</Td>
-              <Td></Td>
-              <Td>未完了</Td>
-              <Td></Td>
-            </Tr>
-            <Tr>
-              <Td>firestoreの設定 or recoil</Td>
-              <Td>めんどくさそう</Td>
-              <Td>未完了</Td>
-              <Td></Td>
-            </Tr>
-            <Tr>
-              <Td></Td>
-              <Td></Td>
-              <Td>未完了</Td>
-              <Td></Td>
-            </Tr>
           </Tbody>
           <Tfoot>
             <Tr backgroundColor="gray.200">
@@ -115,6 +104,7 @@ export const TodoList = () => {
 
 //まだ形だけ
 //tab切り替えでページ遷移になる？
+//完了未完了のスタイルを無理やりboxに入れてやったけどましな方法はないのか
 
 //参考
 //https://qiita.com/kashimuuuuu/items/0cc99820d120aae473fe#%E6%BA%96%E5%82%99firebase%E3%81%AE%E3%83%97%E3%83%AD%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88%E3%82%92%E4%BD%9C%E6%88%90%E3%81%99%E3%82%8B
